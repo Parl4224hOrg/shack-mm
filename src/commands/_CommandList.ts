bcc009fe5103131f55ec2c8c0730c551feff8abd
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-06-16 10:58:59 -0400

    Update _CommandList.ts

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -29,11 +29,10 @@ import {unregisterServer} from "./admin/unregisterServer";
 import {ping} from "./ping";
 import {fixCDs} from "./admin/fixCDs";
 import {unmute} from "./admin/unmute";
-import {updateMatchScore} from "./moderator/updateMatchScore";
 
 const commandList: Command[] = [sync, lfg, prepare, _queue, _ready, unready, pingPlayers, abandon,
     stats, manualSubmit, graph, register, ratingChange, pingMe, echo, games, setRequeue, checkBan,
-    help, checkDms, dmOptions, _mod, serverStatus, late, categoryDelete, unregisterServer, ping, fixCDs, unmute, updateMatchScore];
+    help, checkDms, dmOptions, _mod, serverStatus, late, categoryDelete, unregisterServer, ping, fixCDs, unmute];
 let CommandMap: Collection<string, Command> = new Collection<string, Command>();
 
 
