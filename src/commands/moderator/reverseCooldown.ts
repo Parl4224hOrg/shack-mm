10c45d2607ed917737e0fbfb6d5fb67bad1c6661
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-06-16 22:52:10 -0400

    Update reverseCooldown.ts

--- src/commands/moderator/reverseCooldown.ts
+++ src/commands/moderator/reverseCooldown.ts
@@ -7,6 +7,7 @@ import {Actions} from "../../database/models/ActionModel";
 import {getUserByUser} from "../../modules/getters/getUser";
 import {updateUser} from "../../modules/updaters/updateUser";
 import {SlashCommandSubcommandBuilder} from "discord.js";
+import {Client, EmbedBuilder, TextChannel} from "discord.js";
 
 export const reverseCooldown: SubCommand = {
     data: new SlashCommandSubcommandBuilder()
@@ -17,6 +18,7 @@ export const reverseCooldown: SubCommand = {
         .addStringOption(reason),
     run: async (interaction, data) => {
         try {
+            let reason = interaction.options.getString('reason', true);
             const dbUser = await getUserByUser(interaction.options.getUser('user', true), data);
             if (interaction.options.getString('type', true) == 'abandon') {
                 dbUser.banCounterAbandon--;
@@ -36,10 +38,16 @@ export const reverseCooldown: SubCommand = {
             if (interaction.channel?.type === ChannelType.GuildPublicThread ||
                 interaction.channel?.type === ChannelType.GuildPrivateThread ||
                 interaction.channel?.type === ChannelType.GuildNewsThread) {
-                await interaction.reply({ephemeral: false, content: `<${dbUser.username}> cooldown of reversed`});
+                await interaction.reply({ephemeral: false, content: `<${dbUser.username}> cooldown reversed`});
             } else {
-                await interaction.reply({ephemeral: false, content: `<@${dbUser.id}> cooldown of reversed`});
+                await interaction.reply({ephemeral: false, content: `<@${dbUser.id}> cooldown reversed`});
             }
+            const channel = await interaction.client.channels.fetch(tokens.ModeratorLogChannel) as TextChannel;
+            const embed = new EmbedBuilder();
+            embed.setTitle(`User ${user.username} cooldown reversed`);
+            embed.setDescription(`<@${user.id}> cooldown reversed by <@${interaction.user.id}> because: ${reason}`);
+            await channel.send({embeds: [embed.toJSON()]});
+            await interaction.reply({ephemeral: true, content: `<@${user.id}> has been cooldown reversed`});
         } catch (e) {
             await logError(e, interaction);
         }
