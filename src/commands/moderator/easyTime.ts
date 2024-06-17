cc5e58873ead679d568188879e66761e506118ab
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-06-17 11:06:28 -0400

    Update easyTime.ts

--- src/commands/moderator/easyTime.ts
+++ src/commands/moderator/easyTime.ts
@@ -22,10 +22,11 @@ export const easyTime: SubCommand = {
             } else {
                 const timestamp = game.finalGenTime + 10 * 60;
                 const message = interaction.options.getString('message');
+                await interaction.reply({ ephemeral: true, content: "easy_time is working" });
                 if (message) {
-                    await interaction.reply({ephemeral: false, content: message.replace('{user}', `<@${user.id}>`).replace("{time}", `<t:${timestamp}:R>`)});
+                    await interaction.followUp({ephemeral: false, content: message.replace('{user}', `<@${user.id}>`).replace("{time}", `<t:${timestamp}:R>`)});
                 } else {
-                    await interaction.reply({ephemeral: false, content: `<@${user.id}> <t:${timestamp}:R> you will be abandoned if you do not join the game`});
+                    await interaction.followUp({ephemeral: false, content: `<@${user.id}> <t:${timestamp}:R> you will be abandoned if you do not join the game`});
                 }
             }
         } catch (e) {
