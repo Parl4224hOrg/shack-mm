4d4caf9a4a71be29daeb096f138b683e9ef420dc
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2024-06-16 22:58:14 -0500

    make toggle ref take a boolean instead of a toggle

--- src/commands/moderator/toggleReferee.ts
+++ src/commands/moderator/toggleReferee.ts
@@ -1,5 +1,5 @@
 import {SubCommand} from "../../interfaces/Command";
-import {SlashCommandSubcommandBuilder} from "discord.js";
+import {SlashCommandBooleanOption, SlashCommandSubcommandBuilder} from "discord.js";
 import {userOption} from "../../utility/options";
 import {logError} from "../../loggers";
 import tokens from "../../tokens";
@@ -10,15 +10,15 @@ export const toggleReferee: SubCommand = {
     data: new SlashCommandSubcommandBuilder()
         .setName('toggle_referee')
         .setDescription('Toggles whether a user is a referee or not')
-        .addUserOption(userOption("User to toggle status of")),
+        .addUserOption(userOption("User to toggle status of"))
+        .addBooleanOption(new SlashCommandBooleanOption()
+            .setName('is_ref')
+            .setDescription('Whether the user should be made a ref or have it removed')
+            .setRequired(true)),
     run: async (interaction, data) => {
         try {
             const dbUser = await getUserByUser(interaction.options.getUser('user', true), data);
-            if (dbUser.referee == null) {
-                dbUser.referee = true
-            } else {
-                dbUser.referee = !dbUser.referee
-            }
+            dbUser.referee = interaction.options.getBoolean('is_ref', true);
             await updateUser(dbUser, data);
             await interaction.reply({ephemeral: true, content: `<@${dbUser.id}> ${dbUser.referee ? "is now" : "is no longer"} as referee.`});
         } catch (e) {
