ce2e110d9ffa20118e2db9ae54d0d7167bcd8196
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-06-16 22:57:22 -0400

    Update adjustMMR.ts

--- src/commands/moderator/adjustMMR.ts
+++ src/commands/moderator/adjustMMR.ts
@@ -6,6 +6,7 @@ import {getStats} from "../../modules/getters/getStats";
 import {updateStats} from "../../modules/updaters/updateStats";
 import tokens from "../../tokens";
 import {SlashCommandSubcommandBuilder} from "discord.js";
+import {Client, EmbedBuilder, TextChannel} from "discord.js";
 
 export const adjustMMR: SubCommand = {
     data: new SlashCommandSubcommandBuilder()
@@ -15,6 +16,7 @@ export const adjustMMR: SubCommand = {
         .addNumberOption(option => option.setName('mmr_delta').setDescription('Amount to change MMR by').setRequired(true)),
     run: async (interaction, data) => {
         try {
+            let reason = interaction.options.getString('reason', true);
             const dbUser = await getUserByUser(interaction.options.getUser('user', true), data);
             const stats = await getStats(dbUser._id, "SND");
             const mmrDelta = interaction.options.getNumber('mmr_delta', true);
@@ -24,6 +26,11 @@ export const adjustMMR: SubCommand = {
             stats.mmr += mmrDelta;
             await updateStats(stats);
             await interaction.reply({content: `<@${dbUser.id}>'s MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}.` });
+            const channel = await interaction.client.channels.fetch(tokens.ModeratorLogChannel) as TextChannel;
+            const embed = new EmbedBuilder();
+            embed.setTitle(`User ${dbUser.username} has been MMR adjusted`);
+            embed.setDescription(`<@${dbUser.id}> MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}. Done by <@${interaction.user.id}>`);
+            await channel.send({embeds: [embed.toJSON()]});
         } catch (e) {
             await logError(e, interaction);
         }
