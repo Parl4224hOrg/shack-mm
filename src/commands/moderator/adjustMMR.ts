619acfd25afa8974b88acb27ee6743e578680535
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-06-19 15:13:57 -0400

    Update adjustMMR.ts

--- src/commands/moderator/adjustMMR.ts
+++ src/commands/moderator/adjustMMR.ts
@@ -16,38 +16,22 @@ export const adjustMMR: SubCommand = {
         .addNumberOption(option => option.setName('mmr_delta').setDescription('Amount to change MMR by').setRequired(true)),
     run: async (interaction, data) => {
         try {
-            const logChannel = await interaction.client.channels.fetch(tokens.LogChannel) as TextChannel;
-            
-            await interaction.deferReply({ ephemeral: true });
-            let reason = interaction.options.getString('reason', true);
             const dbUser = await getUserByUser(interaction.options.getUser('user', true), data);
             const stats = await getStats(dbUser._id, "SND");
             const mmrDelta = interaction.options.getNumber('mmr_delta', true);
-
-            const logEmbed = new EmbedBuilder();
-            logEmbed.setTitle(`Adjust MMR logging`);
-            logEmbed.setDescription(`line 29`);
-            await logChannel.send({embeds: [logEmbed.toJSON()]});
-            
             for (let mmr of stats.mmrHistory) {
                 mmr += mmrDelta;
             }
-            
-            logEmbed.setDescription(`line 38`);
-            await logChannel.send({embeds: [logEmbed.toJSON()]});
-            
             stats.mmr += mmrDelta;
             await updateStats(stats);
-
-            logEmbed.setDescription(`line 46`);
-            await logChannel.send({embeds: [logEmbed.toJSON()]});
-            
-            await interaction.followUp({content: `<@${dbUser.id}>'s MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}.` });
-            const channel = await interaction.client.channels.fetch(tokens.ModeratorLogChannel) as TextChannel;
-            const embed = new EmbedBuilder();
-            embed.setTitle(`User ${dbUser.id} has been MMR adjusted`);
-            embed.setDescription(`<@${dbUser.id}> MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}. Done by <@${interaction.user.id}>`);
-            await channel.send({embeds: [embed.toJSON()]});
+            await interaction.reply({content: `<@${dbUser.id}>'s MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}.` });
+            if(mmrDelta !== 0) {
+                const channel = await interaction.client.channels.fetch(tokens.ModeratorLogChannel) as TextChannel;
+                const embed = new EmbedBuilder();
+                embed.setTitle(`User ${dbUser.id} has been MMR adjusted`);
+                embed.setDescription(`<@${dbUser.id}> MMR has been adjusted by ${mmrDelta}. New MMR is ${stats.mmr}. Done by <@${interaction.user.id}>`);
+                await channel.send({embeds: [embed.toJSON()]});
+            }
         } catch (e) {
             await logError(e, interaction);
         }
