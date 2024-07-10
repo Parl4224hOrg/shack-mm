9a81fcc7181f2d5639a67ee3758a5e6fafa4c848
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-07-09 21:34:52 -0400

    Update staticViews.ts

--- src/views/staticViews.ts
+++ src/views/staticViews.ts
@@ -8,6 +8,7 @@ import {stats} from "../buttons/queue/stats";
 import {games} from "../buttons/queue/games";
 import {APAC, EUE, EUW, NAE, NAW} from "../buttons/regionSelect";
 import {pingMeButton} from "../buttons/queue/pingMe";
+import {checkBanButton} from "../buttons/queue/checkBan";
 
 export const sndFILLReadyView = () => {
     return new ActionRowBuilder<MessageActionRowComponentBuilder>()
@@ -16,7 +17,7 @@ export const sndFILLReadyView = () => {
 
 export const SNDFILLReadyView2 = () => {
     return new ActionRowBuilder<MessageActionRowComponentBuilder>()
-        .addComponents(lfg.data, games.data, stats.data, pingMeButton.data).toJSON();
+        .addComponents(lfg.data, games.data, stats.data, pingMeButton.data, checkBanButton.data).toJSON();
 }
 
 
