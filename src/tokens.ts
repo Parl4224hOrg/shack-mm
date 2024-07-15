7b09ca2ea08ab7a0a6e73ffd70b470fdefb297ad
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-07-15 01:23:20 -0400

    Update tokens.ts

--- src/tokens.ts
+++ src/tokens.ts
@@ -71,7 +71,7 @@ export default {
         Autumn: "https://shackmm.com/static/images/manor.png",
     },
     ReductionGames: 10,
-    MapPool: ["Dust 2", "Cache", "Overpass", "Harbor", "Reachsky", "Autumn", "Vertigo", "Industry", "Inferno", "Mirage"],
+    MapPool: ["Dust 2", "Cache", "Overpass", "Harbor", "Manor", "Autumn", "Vertigo", "Industry", "Inferno", "Mirage"],
     PingToPlayTime: 90 * 60,
     Ranks: [
         {name: 'Master', threshold: 1950, roleId: '1152692826669326398'},
