c804be5130514a8c50014ce9511579bf10b63739
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2024-08-20 09:39:03 -0400

    Update tokens.ts

--- src/tokens.ts
+++ src/tokens.ts
@@ -71,7 +71,7 @@ export default {
         Whitesand: "https://shackmm.com/static/images/manor.png",
     },
     ReductionGames: 10,
-    MapPool: ["Dust 2", "Cache", "Overpass", "Harbor", "Inferno", "Autumn", "Vertigo", "Mirage", "Reachsky"],
+    MapPool: ["Dust 2", "Cache", "Overpass", "Whitesand", "Inferno", "Reachsky", "Vertigo", "Mirage", "Harbor"],
     PingToPlayTime: 90 * 60,
     Ranks: [
         {name: 'Master', threshold: 1950, roleId: '1152692826669326398'},
