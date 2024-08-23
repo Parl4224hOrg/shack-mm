f51b06c75852f9f29481eff08d0e6f4bf0feab2f
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2024-08-22 20:51:42 -0500

    add abandon button to views

--- src/views/acceptView.ts
+++ src/views/acceptView.ts
@@ -4,10 +4,11 @@ import {MessageActionRowComponentBuilder} from "discord.js";
 import {missing} from "../buttons/match/missing";
 import {abandonConfirm} from "../buttons/match/abandon/abandonConfirm";
 import {abandonDeny} from "../buttons/match/abandon/abandonDeny";
+import {abandonButton} from "../buttons/match/abandon/abandonButton";
 
 export const acceptView = () => {
     return new ActionRowBuilder<MessageActionRowComponentBuilder>()
-        .addComponents(accept.data, missing.data).toJSON();
+        .addComponents(accept.data, missing.data, abandonButton.data).toJSON();
 }
 
 export const confirmAbandonView = () => {
