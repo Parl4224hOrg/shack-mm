72f118369233cbefb4e17f3bcdad3a09a5292324
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2024-10-02 00:54:36 -0500

    comment out command that prevent building

--- src/commands/admin/newSaveTest.ts
+++ src/commands/admin/newSaveTest.ts
@@ -10,30 +10,30 @@ export const newSaveTest: Command = {
         .setName("new-save-test")
         .setDescription("Tests the new save system"),
     run: async (interaction, data) => {
-        try {
-            await interaction.deferReply({ephemeral: true});
-            const game = data.getQueue().activeGames[0];
-            if (game) {
-                const serializedData = Serializer.serializeGame(game);
-                const save = await SaveV2Model.create({
-                    id: new Date().getTime(),
-                    data: serializedData,
-                });
-                let replyMessage = `Successfully saved data with id: ${save.id}\n`;
-                try {
-                    await Serializer.deserializeGame(save.data, interaction.client, data);
-                    replyMessage += "Successfully loaded data as well";
-                } catch (e) {
-                    const temp = e as any
-                    replyMessage += `Unsuccessfully loaded data with error: ${temp.message}`;
-                }
-                await interaction.followUp({ephemeral: true, content: replyMessage});
-            } else {
-                await interaction.followUp({ephemeral: true, content: "No active game to test serialization with"})
-            }
-        } catch (e) {
-            await logError(e, interaction);
-        }
+        // try {
+        //     await interaction.deferReply({ephemeral: true});
+        //     const game = data.getQueue().activeGames[0];
+        //     if (game) {
+        //         const serializedData = Serializer.serializeGame(game);
+        //         const save = await SaveV2Model.create({
+        //             id: new Date().getTime(),
+        //             data: serializedData,
+        //         });
+        //         let replyMessage = `Successfully saved data with id: ${save.id}\n`;
+        //         try {
+        //             await Serializer.deserializeGame(save.data, interaction.client, data);
+        //             replyMessage += "Successfully loaded data as well";
+        //         } catch (e) {
+        //             const temp = e as any
+        //             replyMessage += `Unsuccessfully loaded data with error: ${temp.message}`;
+        //         }
+        //         await interaction.followUp({ephemeral: true, content: replyMessage});
+        //     } else {
+        //         await interaction.followUp({ephemeral: true, content: "No active game to test serialization with"})
+        //     }
+        // } catch (e) {
+        //     await logError(e, interaction);
+        // }
     },
     name: 'new-save-test',
     allowedRoles: [tokens.LeadModRole, tokens.AdminRole],
