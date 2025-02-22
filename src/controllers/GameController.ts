c89cb0e00006e624b5dfe3c6336cdc742e664450
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-02-22 16:37:16 -0600

    fix bug

--- src/controllers/GameController.ts
+++ src/controllers/GameController.ts
@@ -976,7 +976,11 @@ export class GameController {
 
             const rolePromises = this.users.map(async (user) => {
                 const member = await getGuildMember(user.discordId, this.guild);
-                return member.roles.add(teamARole); // Requests are initiated simultaneously
+                if (user.team == 0) {
+                    return member.roles.add(teamARole)
+                } else {
+                    return member.roles.add(teamBRole)
+                }
             });
             await Promise.all(rolePromises);
 
