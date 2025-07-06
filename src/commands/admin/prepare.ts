5aaf6809e8cf172a0009c7f8e3283a686991271a
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-07-06 07:00:28 -0600

    Ensure ephemeral flag usage for better message visibility and handle non-sendable channels gracefully

--- src/commands/admin/prepare.ts
+++ src/commands/admin/prepare.ts
@@ -2,7 +2,7 @@ import {Command} from "../../interfaces/Command";
 import {SlashCommandBuilder} from "@discordjs/builders";
 import tokens from "../../tokens";
 import {logError} from "../../loggers";
-import {TextChannel} from "discord.js";
+import {TextChannel, MessageFlagsBitField} from "discord.js";
 import {
     MapTestSignupView,
     regionSelectView,
@@ -32,44 +32,65 @@ export const prepare: Command = {
         ),
     run: async (interaction) => {
         try {
-            await interaction.deferReply({ephemeral: true});
+            await interaction.deferReply({flags: MessageFlagsBitField.Flags.Ephemeral});
             const view = interaction.options.getString('view')!
-            switch (view) {
-                case 'snd_ready': {
-                    await interaction.channel!.send({components: [sndFILLReadyView(), SNDFILLReadyView2(),  SNDFILLReadyView3()], content: 'Ready up for SND'});
-                    await interaction.followUp({ephemeral: true, content: 'prepared snd _ready up view'})
-                } break;
-                case 'signup': {
-                    await interaction.channel!.send({components: [signUpView()], content: tokens.SignUpMessage})
-                    await interaction.followUp({ephemeral: true, content: 'prepared sign up view'})
-                } break;
-                case 'info': {
-                    await interaction.channel!.send({content: tokens.InfoMessage})
-                    await interaction.followUp({ephemeral: true, content: 'prepared info view'})
-                } break;
-                case 'c': {
-                    const roles = await interaction.guild!.roles.fetch();
-                    for (let role of roles.values()) {
-                        if (role.name.includes('team') || role.name.includes('match')) {
-                            await role.delete();
-                        }
+            if (interaction.channel!.isSendable()) {
+                switch (view) {
+                    case 'snd_ready': {
+                        await interaction.channel!.send({
+                            components: [sndFILLReadyView(), SNDFILLReadyView2(), SNDFILLReadyView3()],
+                            content: 'Ready up for SND'
+                        });
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'prepared snd _ready up view'})
                     }
-                    await interaction.followUp({ephemeral: true, content: "done"});
-                } break;
-                case 'c2': {
-                    const channel = interaction.channel as TextChannel;
-                    await channel.bulkDelete(100, true);
-                    await interaction.followUp({ephemeral: true, content: "done"})
-                } break;
-                case 'region': {
-                    await interaction.channel!.send({components: [regionSelectView()], content: tokens.RegionSelectMessage});
-                    await interaction.followUp({ephemeral: true, content: 'prepared region select view'})
-                } break;
-                case 'map': {
-                    await interaction.channel!.send({components: [MapTestSignupView()], content: "Use this button to toggle whether or not to be notified about map tests"});
-                    await interaction.followUp({ephemeral: true, content: 'prepared map testing view'});
-                } break;
-                default: break;
+                        break;
+                    case 'signup': {
+                        await interaction.channel!.send({components: [signUpView()], content: tokens.SignUpMessage})
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'prepared sign up view'})
+                    }
+                        break;
+                    case 'info': {
+                        await interaction.channel!.send({content: tokens.InfoMessage})
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'prepared info view'})
+                    }
+                        break;
+                    case 'c': {
+                        const roles = await interaction.guild!.roles.fetch();
+                        for (let role of roles.values()) {
+                            if (role.name.includes('team') || role.name.includes('match')) {
+                                await role.delete();
+                            }
+                        }
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: "done"});
+                    }
+                        break;
+                    case 'c2': {
+                        const channel = interaction.channel as TextChannel;
+                        await channel.bulkDelete(100, true);
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: "done"})
+                    }
+                        break;
+                    case 'region': {
+                        await interaction.channel!.send({
+                            components: [regionSelectView()],
+                            content: tokens.RegionSelectMessage
+                        });
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'prepared region select view'})
+                    }
+                        break;
+                    case 'map': {
+                        await interaction.channel!.send({
+                            components: [MapTestSignupView()],
+                            content: "Use this button to toggle whether or not to be notified about map tests"
+                        });
+                        await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'prepared map testing view'});
+                    }
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                await interaction.followUp({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'cannot send message'});
             }
         } catch (e) {
             await logError(e, interaction)
