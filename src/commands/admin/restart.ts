c6f63de2b2cf24fa1a71f487db1dc2902a54d253
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-07-08 11:03:03 -0600

    Add restart command for testing

---
+++ src/commands/admin/restart.ts
@@ -0,0 +1,21 @@
+import {Command} from "../../interfaces/Command";
+import {logError} from "../../loggers";
+import tokens from "../../tokens";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {MessageFlagsBitField} from "discord.js";
+
+export const restart: Command = {
+    data: new SlashCommandBuilder()
+        .setName('restart')
+        .setDescription("Restarts the bot"),
+    run: async (interaction) => {
+        try {
+            await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'Restarting bot'});
+            process.exit(1);
+        } catch (e) {
+            await logError(e, interaction);
+        }
+    },
+    name: 'restart',
+    allowedUsers: [tokens.Parl],
+}
