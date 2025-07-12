c99eb4216ed701afa3754565d28eef9b92d66263
dopeheaddude <127546973+dopeheaddude@users.noreply.github.com>
2025-07-12 19:54:53 -0400

    Update getGame.ts

--- src/modules/getters/getGame.ts
+++ src/modules/getters/getGame.ts
@@ -1,5 +1,7 @@
 import GameModel from "../../database/models/GameModel";
 import {ObjectId} from "mongoose";
+import {Client} from "discord.js";
+import {logInfo} from "../../loggers";
 
 export const getGameById = async (gameId: ObjectId) => {
     return GameModel.findOne({_id: gameId});
@@ -11,5 +13,29 @@ export const getGameByMatchId = async (gameId: number) => {
 
 export const getGames = async (rangeStart: number = 0) => {
     return GameModel.find({scoreB: {"$gte": 0}, scoreA: {'$gte': 0}, matchId: {"$gte": rangeStart}});
+}
+
+export const getPreviousGeneratedGame = async (client?: Client) => {
+    const previousGame = await GameModel.find().sort({creationDate: -1}).limit(2).then((games: any[]) => games[1] || null);
+    if (client) {
+        await logInfo(`getPreviousGeneratedGame - Found game: ${previousGame ? `Match ID: ${previousGame.matchId}, Abandoned: ${previousGame.abandoned}` : 'No previous game found'}`, client);
+    }
+    return previousGame;
+}
+
+export const wasUserInPreviousGeneratedGame = async (userId: ObjectId, client?: Client) => {
+    const previousGame = await getPreviousGeneratedGame(client);
+    if (!previousGame) {
+        if (client) {
+            await logInfo(`wasUserInPreviousGeneratedGame - User ${userId}: No previous game found, returning false`, client);
+        }
+        return { wasInGame: false, game: null };
+    }
+    
+    const wasInGame = previousGame.users.some((user: ObjectId) => user.toString() === userId.toString());
+    if (client) {
+        await logInfo(`wasUserInPreviousGeneratedGame - User ${userId}: Match ID ${previousGame.matchId}, Abandoned: ${previousGame.abandoned}, Was in game: ${wasInGame}`, client);
+    }
+    return { wasInGame, game: previousGame };
 }
 
