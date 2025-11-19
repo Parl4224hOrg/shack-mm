073b44ef24b259a8012659b04063e00eead7a4e1
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-11-19 11:18:09 -0600

    make stats button ephemeral

--- src/buttons/queue/stats.ts
+++ src/buttons/queue/stats.ts
@@ -19,7 +19,7 @@ export const stats: Button = {
                 await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, content: "This user has not played enough games to view their stats, you can still see their current available stats by setting the use-old-stats option to true."});
             } else {
                 const image = await generateStatsImage(userStats, interaction.user.displayName);
-                await interaction.reply({files: [image]});
+                await interaction.reply({files: [image], flags: MessageFlagsBitField.Flags.Ephemeral});
             }
         } catch (e) {
             await logError(e, interaction);
