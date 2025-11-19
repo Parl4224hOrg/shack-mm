b94a18053ac2c16efe8ae7d3673d83e7ad3f347a
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-11-19 10:58:13 -0600

    Make new stats view live

--- src/buttons/queue/stats.ts
+++ src/buttons/queue/stats.ts
@@ -4,7 +4,7 @@ import {ButtonStyle, MessageFlagsBitField} from "discord.js";
 import {logError} from "../../loggers";
 import {getUserByUser} from "../../modules/getters/getUser";
 import {getStats} from "../../modules/getters/getStats";
-import {statsEmbed} from "../../embeds/statsEmbed";
+import {generateStatsImage} from "../../utility/stats";
 
 export const stats: Button = {
     data: new ButtonBuilder()
@@ -13,9 +13,14 @@ export const stats: Button = {
         .setCustomId("stats-queue"),
     run: async (interaction, data) => {
         try {
-            const dbUser = await getUserByUser(interaction.user, data);
-            const stats = await getStats(dbUser._id, "SND");
-            await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, embeds: [statsEmbed(stats, dbUser, interaction.user.username,  interaction.user.avatarURL()!)]});
+            const user = await getUserByUser(interaction.user, data);
+            const userStats = await getStats(user._id, "SND");
+            if (userStats.gamesPlayedSinceReset < 10) {
+                await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, content: "This user has not played enough games to view their stats, you can still see their current available stats by setting the use-old-stats option to true."});
+            } else {
+                const image = await generateStatsImage(userStats, interaction.user.displayName);
+                await interaction.reply({files: [image]});
+            }
         } catch (e) {
             await logError(e, interaction);
         }
--- src/commands/queue/stats.ts
+++ src/commands/queue/stats.ts
@@ -1,32 +1,49 @@
 import {Command} from "../../interfaces/Command";
 import {SlashCommandBuilder} from "@discordjs/builders";
-import {userOption} from "../../utility/options";
+import {userOptional} from "../../utility/options";
 import tokens from "../../tokens";
 import {logError} from "../../loggers";
 import {getUserByUser} from "../../modules/getters/getUser";
 import {getStats} from "../../modules/getters/getStats";
 import {statsEmbed} from "../../embeds/statsEmbed";
-import {MessageFlagsBitField} from "discord.js";
+import {MessageFlagsBitField, SlashCommandBooleanOption} from "discord.js";
+import {generateStatsImage} from "../../utility/stats";
 
 export const stats: Command = {
     data: new SlashCommandBuilder()
         .setName('stats')
         .setDescription('Displays a users stats')
-        .addUserOption(userOption('User to display stats of').setRequired(false)),
+        .addUserOption(userOptional('User to display stats of'))
+        .addBooleanOption(new SlashCommandBooleanOption()
+            .setName("use-old-stats")
+            .setDescription("Use old stats embed instead of new stats")
+            .setRequired(false)
+        ),
     run: async (interaction, data) => {
         try {
             let user = interaction.options.getUser('user');
             if (!user) {
                 user = interaction.user;
             }
+            const useOldStats = interaction.options.getBoolean('use-old-stats') ?? false;
             const dbUser = await getUserByUser(user, data);
             // const queueId = interaction.options.getString('queue', true)
             const queueId = "SND";
             // @ts-ignore
             if (queueId != "ALL") {
-                const stats = await getStats(dbUser._id, queueId);
-                const embed = statsEmbed(stats, dbUser, user.username, user.avatarURL()!)
-                await interaction.reply({embeds: [embed]});
+                const userStats = await getStats(dbUser._id, queueId);
+                if (useOldStats) {
+
+                    const embed = statsEmbed(userStats, dbUser, user.username, user.avatarURL()!)
+                    await interaction.reply({embeds: [embed]});
+                } else {
+                    if (userStats.gamesPlayedSinceReset < 10) {
+                        await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, content: "This user has not played enough games to view their stats, you can still see their current available stats by setting the use-old-stats option to true."});
+                    } else {
+                        const image = await generateStatsImage(userStats, user.displayName);
+                        await interaction.reply({files: [image]});
+                    }
+                }
             } else {
                 await interaction.reply({flags: MessageFlagsBitField.Flags.Ephemeral, content: 'getting stats for all is not currently supported'});
             }
